528e668a0350b09ea602368ab8d5d0d764931416
Till <nacsis@posteo.de>
2020-06-15 23:20:35 +0200

    Training on MNIST from torchvision without attack Clean up code Added Latex and pdf

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import argparse
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,90 +6,33 @@ import torchvision as torchvision
 from torchvision import datasets, transforms
 import syft as sy
 from torchvision import transforms, datasets
+import datetime
+import logging
+from pathlib import Path
 
-
+import argparse
 
 ap = argparse.ArgumentParser(description="Comparison framework for attacks on federated learning.")
+ap.add_argument("-d", "--dataset", required=False, default="MNIST", help="Dataset to use",
+                choices=["MNIST", "SVHN", "CIFAR", "ATT"])
+ap.add_argument("-n", "--nodes", required=False, default=2, type=int, help="Amount of nodes")
+ap.add_argument("-bs", "--batch_size", required=False, default=32, type=int, help="Samples per batch")
+ap.add_argument("-es", "--epoch_size", required=False, default=0, type=int,
+                help="Batches per epoch, 0 for the complete set")
+ap.add_argument("-t", "--test_batch_size", required=False, default=100, type=int, help="Samples for test-batch")
+ap.add_argument("-e", "--epochs", required=False, default=3, type=int, help="Amount of epochs")
+ap.add_argument("-lr", "--learning_rate", required=False, default=0.5, type=float, help="Learning rate")
+ap.add_argument("-a", "--attack", required=False, default="DLG", help="Attacks to perform",
+                choices=["GAN", "MI", "UL", "DLG", "iDLG"])
+ap.add_argument("-s", "--seed", required=False, default=1, type=int, help="Integer seed for torch")
 
-#ap.add_argument("-d", "--dataset", required=False, default="MNIST", help="Dataset to use", choices=["MNIST", "SVHN", "CIFAR", "ATT"])
-#ap.add_argument("-a", "--attack", required=False, default="GAN", help="Attacks to perform", choices=["GAN", "MI", "UL", "DLG", "iDLG"])
-#ap.add_argument("-n", "--nodes", required=False, default=10, type=int , help="Amount of noodes")
-#ap.add_argument("-s", "--size", required=False, default=10, type=int, help="Amount of samples per node")
-#ap.add_argument("-lr", "--lrounds", required=False, default=10 , help="Amount of learning rounds")
-#ap.add_argument("-c", "--cpr", required=False, default=10 , help="Amount of clients per learning round")
-#ap.add_argument("-S", "--selective", required=False, default=False , help="Selective up- and download", action='store_true')
-
-#ap.add_argument("-ar", "--arounds", required=False, default=10 , help="Amount of attacking rounds")
-
-args = vars(ap.parse_args())
-
-for arg in args:
-    print(str(arg)+" "+str(args[arg]))
-
-###############################################
-
-
-
-hook = sy.TorchHook(torch)
-bob = sy.VirtualWorker(hook, id="bob")
-alice = sy.VirtualWorker(hook, id="alice")
-
-
-batch_size = 1
-test_batch_size = 100
-epochs = 3
-lr = 0.01
-momentum = 0.5
-no_cuda = False
-seed = 1
-log_interval = 10
-save_model = True
-batches_per_epoch = 100
-
-
-
-use_cuda = not no_cuda and torch.cuda.is_available()
-device = torch.device("cuda")
-
-torch.manual_seed(seed)
-
-transformation = transforms.Compose([
-    transforms.Resize([32, 32]),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-
-test_dataset = datasets.ImageFolder(root='./datasets/datasets/CIFAR-10/test/', transform=transformation)
-train_dataset = datasets.ImageFolder(root='./datasets/datasets/CIFAR-10/train/', transform=transformation)
-
-#train_dataset = datasets.MNIST('./datasets', train=True, download=True, transform=transformation)
-#test_dataset = datasets.MNIST('./datasets', train=False, download=True, transform=transformation)
-
-
-
-federated_train_loader = sy.FederatedDataLoader(
-    train_dataset.federate((bob, alice)),
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers= 0,
-    pin_memory=True
-)
-
-test_loader = torch.utils.data.DataLoader(
-    test_dataset,
-    batch_size=test_batch_size,
-    shuffle=True,
-    num_workers= 0,
-    pin_memory=True
-)
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 20, 5, 1)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500)
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
@@ -99,7 +40,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        x = x.view(-1, 4 * 4 * 50)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
@@ -107,8 +48,9 @@ class Net(nn.Module):
 
 def train():
     model.train()
-    for batch_idx, (data, target) in enumerate(federated_train_loader): # <-- now it is a distributed dataset
-        model.send(data.location) # <-- NEW: send the model to the right location
+
+    for batch_idx, (data, target) in enumerate(federated_train_loader):
+        model.send(data.location)
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
@@ -116,20 +58,16 @@ def train():
         loss.backward()
         optimizer.step()
         model.get()
-        if batch_idx % log_interval == 0:
-            loss = loss.get() # <-- NEW: get the loss back
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * batch_size, batches_per_epoch * batch_size,
-                100* batch_idx / batches_per_epoch, loss.item()))
 
-        if batch_idx >= batches_per_epoch:
+        if (batch_idx % log_interval== 0):
+            loss = loss.get()  # <-- NEW: get the loss back
+            log('Train Epoch: {}, Batch: {}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx , args["epoch_size"], 100 * batch_idx / args["epoch_size"], loss.item()))
+
+        if batch_idx >= args["epoch_size"]:
             break
 
 
-
-
 def test():
-    print("Start Testing")
     model.eval()
     test_loss = 0
     correct = 0
@@ -137,28 +75,104 @@ def test():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(1, keepdim=True) # get the index of the max log-probability
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    log('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
+def log(str):
+    print(str)
+    logging.info(str)
+
 
 if __name__ == '__main__':
-    model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=lr)
+    # get the arguments
+    args = vars(ap.parse_args())
+    argstr = "arg"
+    for arg in args:
+        argstr += ("_" + str(args[arg]))
 
-    for epoch in range(1, epochs + 1):
+    #Hardcoded Parameters
+    log_interval = 10
+
+    # logging
+    time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    resultpath = "results/{}/{}/".format(argstr, time)
+    Path(resultpath).mkdir(parents=True, exist_ok=True)
+    logging.basicConfig(level=logging.DEBUG, filename=resultpath + "log.txt", filemode="w+", format="%(message)s")
+    log("Model and Log will be saved to: " + resultpath)
+
+    # Log Parameters
+    for arg in args:
+        log('{:15}: {:10}'.format(str(arg), str(args[arg])))
+
+    # Creating Federation Nodes
+    hook = sy.TorchHook(torch)
+    nodes = tuple(sy.VirtualWorker(hook, id=str(id)) for id in range(0, args["nodes"]))
+
+    # Check CUDA
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        log("Torch successfully connected to CUDA device: {}".format(torch.cuda.current_device()))
+    else:
+        log("Error: Torch can't connect to CUDA")
+        exit()
+
+    # Setting Torch Seed
+    torch.manual_seed(args["seed"])
+
+    # Defining data transformation
+    transformation = transforms.Compose([
+        # transforms.Resize([32, 32]),
+        transforms.ToTensor()  # ,
+        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # Initialising datasets
+    train_dataset = datasets.MNIST('./datasets', train=True, download=True, transform=transformation)
+    test_dataset = datasets.MNIST('./datasets', train=False, download=True, transform=transformation)
+
+    # Setting epoch_size if 0 (complete set) is choosen
+    if args["epoch_size"] == 0:
+        args["epoch_size"] = len(train_dataset) / args["batch_size"]
+        log("Set epoch size to {}".format(args["epoch_size"]))
+
+    # Initialise Federated Data loader for training
+    federated_train_loader = sy.FederatedDataLoader(
+        train_dataset.federate(nodes),
+        batch_size=args["batch_size"],
+        shuffle=True,
+        num_workers=0,
+        pin_memory=True
+    )
+
+    # Initialise Data Loader for testing
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=args["test_batch_size"],
+        shuffle=True,
+        num_workers=0,
+        pin_memory=True
+    )
+
+    # Initialise Model
+    model = Net().to(device)
+
+    # Initialise Optimizer
+    optimizer = optim.SGD(model.parameters(), lr=args["learning_rate"])
+
+    # Training Loop
+    for epoch in range(1, args["epochs"] + 1):
         train()
 
-    print("Training finished")
-
+    # Testing
     test()
 
-    if (save_model):
-        torch.save(model.state_dict(), "model.pt")
+    # Saving Model
+    torch.save(model.state_dict(), "{}model.pt".format(resultpath))
