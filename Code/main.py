1503d9eb9b454cf5de6f00935fbf295fc65f216c
till.muessig <nacsis@posteo.de>
2020-08-11 11:55:33 +0200

    ex 2

--- Code/main.py
+++ Code/main.py
@@ -11,10 +11,10 @@ def main():
     # setting, graph = prediction_accuracy_vs_batchsize(10, [1, 2, 4, 6, 16, 32, 64, 128, 256], "CIFAR", False)
 
     ### Experiment 2: Good Fidelity ###
-    setting, graph = good_fidelity(10, 4, 60, "MNIST", True)
-    setting, graph = good_fidelity(10, 4, 60, "MNIST", False)
-    setting, graph = good_fidelity(10, 4, 60, "CIFAR", True)
-    setting, graph = good_fidelity(10, 4, 60, "CIFAR", False)
+    setting, graph = good_fidelity(4, 4, 300, "MNIST", True)
+    setting, graph = good_fidelity(4, 4, 300, "MNIST", False)
+    setting, graph = good_fidelity(4, 4, 300, "CIFAR", True)
+    setting, graph = good_fidelity(4, 4, 300, "CIFAR", False)
 
     ############################################################
     print("Run finished")
