0f41f27a3d8dbdf06b0b888f775425406a4e9eee
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2021-03-29 16:50:06 +0200

    update experiment params and call

--- Code/main.py
+++ Code/main.py
@@ -6,69 +6,124 @@ from Dataloader import Dataloader
 def main():
     ############## Build your attack here ######################
 
-    dlg_iterations = [100]
-    datasets = ["MNIST"] #, "CIFAR", "CELEB-A-male", "SVHN"]
-    # datasets = ["SVHN", "CELEB-A-male", "CIFAR", "MNIST"]
-    v3 = {"MNIST": "v3-zero", "CIFAR": "v3-one", "CELEB-A-male": "v3-zero", "SVHN": "v3-random"}
-
-    job = "custom-experiment"
-    # job = "visualize"
+    experiment_set = 1
 
     # visualization parameters
-    experiment_set = 1
+    job = "visualize"
+    # specify the number of json files you want to select for plotting
     num_files = 1
+
+    # experiment parameters
+    # FIXME: outcomment this line if you want the run the visualization
+    job = "experiment"
+
+    # defaults to fill vars, when unused
+    train_lr = 0.1
+    defenses = ["none"]
+    differential_privacy = False
+    noise_type = "normal"
+    noise_multipliers = [0.0]
+    compression = False
+    thresholds = [0.0]
+
+    # with exception of set 1 and 3 all sets use non-IID
+    balanced = False
+
+    if experiment_set in [1, 3]:
+        balanced = True
+
+    # with exception of set 1, 2, 3, and 4 all sets use MNIST and LLG+
     dataset = "MNIST"
-    # dataset = "CIFAR"
-    # dataset = "CELEB-A-male"
-    # dataset = "SVHN"
+    version = "v2"
 
-    if job == "custom-experiment":
+    v3 = {"MNIST": "v3-zero", "CIFAR": "v3-one", "CELEB-A-male": "v3-zero", "SVHN": "v3-random"}
 
+    if experiment_set in [1, 2, 3, 4]:
+        # TODO: run all DATASETS separately ("in parallel")
+        dataset = "MNIST"
+        # dataset = "CIFAR"
+        # dataset = "CELEB-A-male"
+        # dataset = "SVHN"
+        # versions in order of importance
+        # TODO: run all VERSIONS separately per DATASET ("in parallel")
+        version = "v2"
+        # version = "random"
+        # version = "v1"
+        # version = v3[dataset]
+
+    # with exception of set 3 and 4 all sets use all batch sizes and don't train
+    list_bs = [1,2,4,8,16,32,64,128]
+    trainsize = 0
+    trainsteps = 0
+
+    # Set 3 and 4 generation
+    if experiment_set in [3, 4]:
+        list_bs = [8]
+        trainsize = 100
+        trainsteps = 100
+        # TODO: ask Aidmar about the LEARNING RATE
+        # we use 0.1 as the default, but we also ran some experiments with 0.01
+        # I don't remember which results we ended up using for the paper.
+        # train_lr = 0.01
+
+    # with exception of set 5 all sets use the ConvNet model (old LeNet)
+    model = "LeNet"
+
+    # Set 5 generation
+    if experiment_set == 5:
+        # TODO: run all MODELS separately ("in parallel")
+        # model = "LeNet"       # old LeNet = ConvNet
+        model = "NewNewLeNet"   # new LeNet = LeNet
+        # model = "ResNet"      # ResNet
+        # model = "MLP"         # FCNN
+
+    # TODO: You can use the old LeNet run form set 5 as a baseline for sets 5, 6, and 7
+
+    # Set 6 generation
+    if experiment_set == 6:
+        defenses = ["dp"]
+        differential_privacy = True
+        # TODO: run all NOISE TYPES separately ("in parallel")
+        noise_type = "normal"
+        # noise_type = "laplace"
+        # noise_type = "exponential"
+        noise_multipliers = [0.1, 0.01, 0.001, 0.0001]
+
+    # Set 7 generation
+    if experiment_set == 7:
+        defenses = ["compression"]
+        compression = True
+        thresholds = [0.1, 0.2, 0.4, 0.8]
+
+    if job == "experiment":
         dataloader = Dataloader()
-        for dataset in datasets:
-            experiment(dataloader=dataloader,
-                    list_datasets=[dataset],
-                    list_bs=[8],
-                    list_balanced=[False],
-                    list_versions=["v2"],   # "v1"(LLG), "v2"(LLG+), "v3-zero", "v3-one", "v3-random", "dlg", "idlg"
-                    n=100,                     # Amount of attacks
-                    extent="predict",        # "victim_side", "predict", "reconstruct"
-                    trainsize=0,#10000,             # Iterations per Trainstep
-                    trainsteps=0,           # Number of Attack&Train cycles
-                    path=None,
-                    model="LeNet",
-                    store_individual_gradients=False, # Will store the ~500 gradients connected to one output node and not just their sum
-                    dlg_lr= 1, # learrate of (i)dlg image reconstruction
-                    dlg_iterations=100, # amount of (i)dlg reconstruction iterations
-                    log_interval=100000,  # Won't store each (i)dlg iteration's images but every n-th iteration's
-                    store_composed_image = False, # storing dlg output images as composed image
-                    store_separate_images = False, # storing dlg output images as seperate images
-                    defenses=["dp"],
-                    differential_privacy=True,
-                    noise_type="normal",
-                    noise_multiplier=0.1
-
-                    )
-
-    # Set 1 and 2 generation
-    elif job == "Untrained_MNIST-experiment":
-        print("todo")
-    elif job == "Untrained_CIFAR-experiment":
-        print("todo")
-    elif job == "Untrained_CELEB-A-experiment":
-        print("todo")
-    elif job == "Untrained_CIFAR-experiment":
-        print("todo")
-
-    # Set 3 generation
-    elif job == "Trained_MNIST-experiment":
-        print("todo")
-    elif job == "Trained_CIFAR-experiment":
-        print("todo")
-    elif job == "Trained_CELEB-A-experiment":
-        print("todo")
-    elif job == "Trained_CIFAR-experiment":
-        print("todo")
+        for noise_multiplier in noise_multipliers:
+            for threshold in thresholds:
+                experiment(dataloader=dataloader,
+                        list_datasets=[dataset],
+                        list_bs=list_bs,
+                        list_balanced=[balanced],
+                        list_versions=[version],   # "v1"(LLG), "v2"(LLG+), "v3-zero", "v3-one", "v3-random", "dlg", "idlg"
+                        n=100,                     # Amount of attacks
+                        extent="predict",        # "victim_side", "predict", "reconstruct"
+                        trainsize=trainsize,             # Iterations per Trainstep
+                        trainsteps=trainsteps,           # Number of Attack&Train cycles
+                        train_lr=train_lr,
+                        path=None,
+                        model=model,
+                        store_individual_gradients=False, # Will store the ~500 gradients connected to one output node and not just their sum
+                        dlg_lr= 1, # learrate of (i)dlg image reconstruction
+                        dlg_iterations=100, # amount of (i)dlg reconstruction iterations
+                        log_interval=100000,  # Won't store each (i)dlg iteration's images but every n-th iteration's
+                        store_composed_image = False, # storing dlg output images as composed image
+                        store_separate_images = False, # storing dlg output images as seperate images
+                        defenses=defenses,
+                        differential_privacy=differential_privacy,
+                        noise_type=noise_type,
+                        noise_multiplier=noise_multiplier,
+                        compression=compression,
+                        threshold=threshold
+                        )
 
     elif job == "visualize":
 
