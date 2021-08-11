ca8f7462dcbeaf69ed01765771e1ea805fdb587b
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2021-08-11 10:27:57 +0200

    get num_classes dynamically from dataset in Dataloader
    remove hardcoded num_classes
    ensure num_classes is integer

--- Code/Dataloader.py
+++ Code/Dataloader.py
@@ -22,35 +22,26 @@ class Dataloader():
 
         if dataset == "MNIST":
             self.train_dataset = datasets.MNIST('./datasets', train=True, download=True, transform=tt)
-            self.num_classes = 10
         elif dataset == "EMNIST":
             self.train_dataset = datasets.EMNIST('./datasets', split="byclass", train=True, download=True, transform=tt)
-            self.num_classes = 62
         elif dataset == "FEMNIST":
             self.train_dataset = femnist.FEMNIST('./datasets', train=True, download=True, transform=tt)
-            self.num_classes = 62
         elif dataset == "FEMNIST-digits":
             self.train_dataset = femnist.FEMNIST_digits('./datasets', train=True, download=True, transform=tt)
-            self.num_classes = 10
         elif dataset == 'CIFAR':
             self.train_dataset = datasets.CIFAR100('./datasets', train=True, download=True, transform=tt)
-            self.num_classes = 100
         elif dataset == 'CIFAR-grey':
             self.train_dataset = datasets.CIFAR100('./datasets', train=True, download=True, transform=tt_grey)
-            self.num_classes = 100
         elif dataset == 'CELEB-A':
             self.train_dataset = datasets.CelebA('./datasets', 'all', 'identity', download=True, transform=tt)
-            self.num_classes = 100
             self.train_dataset.targets = self.train_dataset.identity
         elif dataset == 'CELEB-A-male':
             self.train_dataset = datasets.CelebA('./datasets', 'all', 'attr', download=True, transform=tt)
-            self.num_classes = 2
             #filtering male from attributes and set it as target
             length = len(self.train_dataset.attr)
             self.train_dataset.targets = torch.gather(self.train_dataset.attr, 1, torch.Tensor(length*[20]).long().view(-1, 1))
         elif dataset == 'CELEB-A-hair':
             self.train_dataset = datasets.CelebA('./datasets', 'all', 'attr', download=True, transform=tt)
-            self.num_classes = 5
             length = len(self.train_dataset.attr)
             self.train_dataset.targets = []
 
@@ -72,10 +63,18 @@ class Dataloader():
 
         elif dataset == 'SVHN':
             self.train_dataset = datasets.SVHN('./datasets', 'train', download=True, transform=tt)
-            self.num_classes = 10
             self.train_dataset.targets = self.train_dataset.labels.tolist()
         else:
             print("Unsupported dataset '" + dataset + "'")
+            exit()
+
+        # number of classes
+        if isinstance(self.train_dataset.targets, torch.Tensor):
+            self.num_classes = int(max(self.train_dataset.targets).item() + 1)
+        elif isinstance(self.train_dataset.targets, list):
+            self.num_classes = int(max(self.train_dataset.targets) + 1)
+        else:
+            print("Unsupported target type '" + type(self.train_dataset.targets) + "'")
             exit()
 
         self.currently_loaded = dataset
