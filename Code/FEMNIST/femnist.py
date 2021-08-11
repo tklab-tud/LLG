714128ce4261e29223eb9e8ca2a7430bfec8b578
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2021-08-11 03:10:48 +0200

    add full FEMNIST WiP
    TODO: files download

--- Code/FEMNIST/femnist.py
+++ Code/FEMNIST/femnist.py
@@ -62,3 +62,65 @@ class FEMNIST_digits(MNIST):
         print('Processing...')
         shutil.move(os.path.join(self.raw_folder, self.training_file), self.processed_folder)
         shutil.move(os.path.join(self.raw_folder, self.test_file), self.processed_folder)
+
+
+class FEMNIST(MNIST):
+    """
+    This dataset is derived from the Leaf repository
+    (https://github.com/TalwalkarLab/leaf) pre-processing of the Extended MNIST
+    dataset, grouping examples by writer. Details about Leaf were published in
+    "LEAF: A Benchmark for Federated Settings" https://arxiv.org/abs/1812.01097.
+    """
+    resources = [
+        ('https://s3.amazonaws.com/nist-srd/SD19/by_write.zip',
+         'a29f21babf83db0bb28a2f77b2b456cb'),
+        ('https://s3.amazonaws.com/nist-srd/SD19/by_class.zip',
+         '79572b1694a8506f2b722c7be54130c4')]
+
+    def __init__(self, root, train=True, transform=None, target_transform=None,
+                 download=False):
+        super(MNIST, self).__init__(root, transform=transform,
+                                    target_transform=target_transform)
+        self.train = train
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found.' +
+                               ' You can use download=True to download it')
+        if self.train:
+            data_file = self.training_file
+        else:
+            data_file = self.test_file
+
+        self.data, self.targets, self.users_index = torch.load(os.path.join(self.processed_folder, data_file))
+
+    def __getitem__(self, index):
+        img, target = self.data[index], int(self.targets[index])
+        img = Image.fromarray(img.numpy(), mode='F')
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return img, target
+
+    def download(self):
+        """Download the FEMNIST data if it doesn't exist in processed_folder already."""
+        import shutil
+
+        if self._check_exists():
+            return
+
+        os.makedirs(self.raw_folder, exist_ok=True)
+        os.makedirs(self.processed_folder, exist_ok=True)
+
+        # download files
+        for url, md5 in self.resources:
+            filename = url.rpartition('/')[2]
+            utils.download_and_extract_archive(url, download_root=self.raw_folder, filename=filename, md5=md5)
+
+        # process and save as torch files
+        print('Processing...')
+        shutil.move(os.path.join(self.raw_folder, self.training_file), self.processed_folder)
+        shutil.move(os.path.join(self.raw_folder, self.test_file), self.processed_folder)
