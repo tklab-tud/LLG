d9d97b69dd8135048d67a28a87bcb5aa10da4b0f
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2021-10-12 16:47:50 +0200

    apply defenses before aggregation

--- Code/Dlg.py
+++ Code/Dlg.py
@@ -13,6 +13,7 @@ class Dlg:
         self.dummy_data = None
         self.dummy_label = None
         self.seperated_gradients = []
+        self.seperated_gradients_def = []
 
     def victim_side(self):
         para = self.setting.parameter
@@ -43,43 +44,46 @@ class Dlg:
         aggregated = list(x.zero_() for x in grad)
 
         #iterate over the gradients for each local iteration
-        for i in self.seperated_gradients:
-            #there iterate through the gradients and add to the aggregator
-            for i_g,g in enumerate(i):
-                aggregated[i_g] = torch.add(aggregated[i_g], g)
+        for grad in self.seperated_gradients:
 
-        # Noisy Gradients
-        if para["differential_privacy"]:
-            clipping = True if self.setting.parameter["max_norm"] != None else False
-            adp.apply_noise(aggregated, para["batch_size"], para["max_norm"], para["noise_multiplier"], para["noise_type"], self.setting.device, loss_reduction="none", clipping=clipping)
+            # Noisy Gradients
+            if para["differential_privacy"]:
+                clipping = True if self.setting.parameter["max_norm"] != None else False
+                adp.apply_noise(grad, para["batch_size"], para["max_norm"], para["noise_multiplier"], para["noise_type"], self.setting.device, loss_reduction="none", clipping=clipping)
 
-        # Gradient Compression
-        if self.setting.parameter["compression"]:
-            values = torch.sum(aggregated[-2], dim=-1).clone()
-            magnitudes = [torch.abs(value) for value in values]
-            magnitudes_sorted = sorted(magnitudes)
+            # Gradient Compression
+            if self.setting.parameter["compression"]:
+                values = torch.sum(grad[-2], dim=-1).clone()
+                magnitudes = [torch.abs(value) for value in values]
+                magnitudes_sorted = sorted(magnitudes)
 
-            threshold = int(len(magnitudes_sorted) * self.setting.parameter["threshold"]) - 1
-            max_magnitude = magnitudes_sorted[threshold]
-            max_mag_count = 1
-            first_idx = threshold
-            for i, mag in enumerate(magnitudes_sorted):
-                if mag == max_magnitude:
-                    first_idx = i
-            max_mag_count = threshold - first_idx
+                threshold = int(len(magnitudes_sorted) * self.setting.parameter["threshold"]) - 1
+                max_magnitude = magnitudes_sorted[threshold]
+                max_mag_count = 1
+                first_idx = threshold
+                for i, mag in enumerate(magnitudes_sorted):
+                    if mag == max_magnitude:
+                        first_idx = i
+                max_mag_count = threshold - first_idx
 
-            count = 0
-            for magnitude, tens in zip(magnitudes, aggregated):
-                if magnitude < max_magnitude:
-                    tens.zero_()
-                elif magnitude == max_magnitude:
-                    if count <= max_mag_count:
+                count = 0
+                for magnitude, tens in zip(magnitudes, grad):
+                    if magnitude < max_magnitude:
                         tens.zero_()
-                    else:
+                    elif magnitude == max_magnitude:
+                        if count <= max_mag_count:
+                            tens.zero_()
+                        else:
+                            continue
+                        count += 1
+                    elif magnitude > max_magnitude:
                         continue
-                    count += 1
-                elif magnitude > max_magnitude:
-                    continue
+
+            self.seperated_gradients_def.append(list((_.detach().clone() for _ in grad)))
+
+            #there iterate through the gradients and add to the aggregator
+            for i_g,g in enumerate(grad):
+                aggregated[i_g] = torch.add(aggregated[i_g], g)
 
 
         self.gradient = list(torch.div(x, 1) for x in aggregated)
