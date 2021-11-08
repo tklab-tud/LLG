81343e441bb09e4286f173cad601521c44ea8134
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2021-11-08 12:30:57 +0100

    move apply and inject into defenses class

--- Code/defenses.py
+++ Code/defenses.py
@@ -25,48 +25,48 @@ class Defenses:
         self.device = setting.device
         self.parameter = setting.parameter
 
-def apply(grad, setting):
-    # Noisy Gradients
-    if setting.parameter["differential_privacy"]:
-        clipping = True if setting.parameter["max_norm"] != None else False
-        adp.apply_noise(grad, setting.parameter["batch_size"], setting.parameter["max_norm"], setting.parameter["noise_multiplier"], setting.parameter["noise_type"], setting.device, loss_reduction="none", clipping=clipping)
+    def apply(self, grad):
+        # Noisy Gradients
+        if self.parameter["differential_privacy"]:
+            clipping = True if self.parameter["max_norm"] != None else False
+            adp.apply_noise(grad, self.parameter["batch_size"], self.parameter["max_norm"], self.parameter["noise_multiplier"], self.parameter["noise_type"], self.device, loss_reduction="none", clipping=clipping)
 
-    # Gradient Compression
-    if setting.parameter["compression"]:
-        values = torch.sum(grad[-2], dim=-1).clone()
-        magnitudes = [torch.abs(value) for value in values]
-        magnitudes_sorted = sorted(magnitudes)
+        # Gradient Compression
+        if self.parameter["compression"]:
+            values = torch.sum(grad[-2], dim=-1).clone()
+            magnitudes = [torch.abs(value) for value in values]
+            magnitudes_sorted = sorted(magnitudes)
 
-        threshold = int(len(magnitudes_sorted) * setting.parameter["threshold"]) - 1
-        max_magnitude = magnitudes_sorted[threshold]
-        max_mag_count = 1
-        first_idx = threshold
-        for i, mag in enumerate(magnitudes_sorted):
-            if mag == max_magnitude:
-                first_idx = i
-        max_mag_count = threshold - first_idx
+            threshold = int(len(magnitudes_sorted) * self.parameter["threshold"]) - 1
+            max_magnitude = magnitudes_sorted[threshold]
+            max_mag_count = 1
+            first_idx = threshold
+            for i, mag in enumerate(magnitudes_sorted):
+                if mag == max_magnitude:
+                    first_idx = i
+            max_mag_count = threshold - first_idx
 
-        count = 0
-        for magnitude, tens in zip(magnitudes, grad):
-            if magnitude < max_magnitude:
-                tens.zero_()
-            elif magnitude == max_magnitude:
-                if count <= max_mag_count:
+            count = 0
+            for magnitude, tens in zip(magnitudes, grad):
+                if magnitude < max_magnitude:
                     tens.zero_()
-                else:
+                elif magnitude == max_magnitude:
+                    if count <= max_mag_count:
+                        tens.zero_()
+                    else:
+                        continue
+                    count += 1
+                elif magnitude > max_magnitude:
                     continue
-                count += 1
-            elif magnitude > max_magnitude:
-                continue
 
-def inject(grads, grad_def, model):
-    params = []
-    for i_g, p in enumerate(model.parameters()):
-        params.append(p)
-        for grad in grads:
-            params[i_g] = torch.sub(params[i_g], grad[i_g])
-        params[i_g] = torch.add(params[i_g], grad_def[i_g])
-    state_dict = model.state_dict()
-    for i, (key, tens) in enumerate(state_dict.items()):
-        state_dict[key] = params[i]
-    model.load_state_dict(state_dict)
+    def inject(self, grads, grad_def, model):
+        params = []
+        for i_g, p in enumerate(model.parameters()):
+            params.append(p)
+            for grad in grads:
+                params[i_g] = torch.sub(params[i_g], grad[i_g])
+            params[i_g] = torch.add(params[i_g], grad_def[i_g])
+        state_dict = model.state_dict()
+        for i, (key, tens) in enumerate(state_dict.items()):
+            state_dict[key] = params[i]
+        model.load_state_dict(state_dict)
